3189aa0d9b83a6ab3d94417d20c52c397a1b1a86
Aditya30december2003 <94634290+Aditya30december2003@users.noreply.github.com>
2023-09-07 07:43:54 +0530

    Create  LUCKYSEVEN.cpp

---
+++ Codechef/SEP23-DIV4/LUCKYSEVEN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[10];
+    for(int i=0;i<10;i++){
+        cin>>str[i];
+    }
+    
+        cout<<str[6];
+    
+
+    return 0;
+}
